07e8716ca46173b29e179272a1ccb44d84b16455
marykhodaparast <maryam.kh1374@gmail.com>
2026-01-08 14:19:32 +0330

    other migrastions

---
+++ database/migrations/central/2026_01_08_103934_create_form_entries_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('form_entries', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('form_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unique('form_id');
+            $table->unique('user_id');
+            $table->json('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('form_entries');
+    }
+};
---
+++ database/migrations/central/2026_01_08_104445_create_themes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('themes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('folder', 191);
+            $table->unique('folder');
+            $table->boolean('active')->default(0);
+            $table->string('version');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('themes');
+    }
+};
